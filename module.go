b555a2a8eb146d3f4e689b95167980c184479bb5
barthr <info@bartfokker.com>
2023-10-13 18:25:33 +0200

    Remove existing modules

--- module.go
+++ module.go
@@ -1,14 +1,8 @@
-package bpfutil
+package libbpfgohelpers
 
 import (
 	"fmt"
 	bpf "github.com/aquasecurity/libbpfgo"
-)
-
-var (
-	// currently we have these as "global" variables since they are used throughout the program
-	DnsProbeModule *bpf.Module
-	DnsTcModule    *bpf.Module
 )
 
 func LoadModuleFromFile(path string) (*bpf.Module, error) {
@@ -22,16 +16,4 @@ func LoadModuleFromFile(path string) (*bpf.Module, error) {
 		return nil, fmt.Errorf("failed loading bpf module on path %s: %w", path, err)
 	}
 	return module, nil
-}
-
-func LoadDnsProbeModule(path string) error {
-	var err error
-	DnsProbeModule, err = LoadModuleFromFile(path)
-	return err
-}
-
-func LoadDnsTcModule(path string) error {
-	var err error
-	DnsTcModule, err = LoadModuleFromFile(path)
-	return err
 }
